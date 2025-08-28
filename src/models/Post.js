0a97b36c04d2af7931d5befef087a23c9e251658
Tien2003deptrai <duongvantiendtu@gmail.com>
2025-08-28 16:57:31 +0700

    Fix: models

--- src/models/Post.js
+++ src/models/Post.js
@@ -1,16 +1,15 @@
-
 import mongoose from 'mongoose';
 
 const PostSchema = new mongoose.Schema(
   {
     title: { type: String, required: true, trim: true, maxlength: 200 },
     content: { type: String, required: true },
-    tags: [{ type: String, trim: true }],
     published: { type: Boolean, default: false }
   },
   { timestamps: true }
 );
 
-PostSchema.index({ title: 'text', content: 'text', tags: 1 });
+// chỉ index full-text cho title và content
+PostSchema.index({ title: 'text', content: 'text' });
 
 export default mongoose.model('Post', PostSchema);
